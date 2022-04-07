4e4f5f15303c3997d1e50a23840492617a3d42f9
choiyoonho <cyh7834@gmail.com>
2022-04-07 21:06:25 +0900

    메소드 이름으로 쿼리 생성

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 @Transactional
 @SpringBootTest
 class MemberRepositoryTest {
@@ -26,4 +28,40 @@ class MemberRepositoryTest {
         Assertions.assertEquals(savedMember, findMember);
     }
 
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        Assertions.assertEquals(findMember1, member1);
+        Assertions.assertEquals(findMember2, member2);
+
+        List<Member> all = memberRepository.findAll();
+        Assertions.assertEquals(all.size(), 2);
+
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        Assertions.assertEquals(memberRepository.count(), 0);
+    }
+
+    @Test
+    public void findByUsernameAndAgeGreaterThan() {
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("AAA", 20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+
+        Assertions.assertEquals(result.get(0).getUsername(), "AAA");
+        Assertions.assertEquals(result.get(0).getAge(), 20);
+        Assertions.assertEquals(result.size(), 1);
+    }
 }
