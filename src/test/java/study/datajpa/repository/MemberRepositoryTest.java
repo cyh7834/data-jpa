e5ef47341224313774cd0ad203094bd36f2fecb4
choiyoonho <cyh7834@gmail.com>
2022-04-12 21:35:33 +0900

    Auditing

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -216,4 +216,9 @@ class MemberRepositoryTest {
         List<Member> result = memberRepository.findLockByUsername("member1");
 
     }
+
+    @Test
+    public void callCustom() {
+        List<Member> memberCustom = memberRepository.findMemberCustom();
+    }
 }
